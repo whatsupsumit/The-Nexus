6c6ecb6dbdeb1cec47f62c41fc362741b0d60f08
Kavish Paraswar <kavishp9721@gmail.com>
2025-10-12 21:53:19 +0530

    Solved ADD FOOTER #2

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,6 +15,7 @@ import { auth } from '../utils/firebase';
 import { addUser, removeUser } from '../utils/userSlice';
 import ProtectedRoute from './ProtectedRoute';
 import Header from './header';
+import Footer from './Footer';
 
 const Body = () => {
   const dispatch = useDispatch();
@@ -378,11 +379,16 @@ const Body = () => {
     return () => unsubscribe();
   }, [dispatch]);
 
-  return (
-    <div>
+    return (
+  <div className="flex flex-col min-h-screen">
+    <div className="flex-grow">
       <RouterProvider router={appRouter} />
     </div>
-  );
+    <Footer />
+  </div>
+);
+
+
 };
 
 export default Body;
